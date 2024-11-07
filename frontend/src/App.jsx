d9e720ee6990ce821e0e3284e14504cdf1ef4b47
simon franco <zaimon88@gmail.com>
2024-11-07 15:16:00 -0500

    Add frontend conf, scss and vitest

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <p className="test">Hello</p>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/vite.config.js
@@ -0,0 +1,20 @@
+// frontend/vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+// import { resolve } from "path";
+// import ssr from "vite-plugin-ssr/plugin";
+
+export default defineConfig({
+  plugins: [react()],
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern", // or "modern"
+      },
+    },
+  },
+  build: {
+    sourcemap: true,
+  },
+});
